8b140aab5abcde1980acb0c94a59ae2779f788de
rumi.kg <rumi.kg@08023214-603c-6ac4-a8a6-c5d31883a6d0>
2010-11-11 08:50:04 +0000

    Adding last lat,lngs by hand.

--- trunk/lng-lat-from-zipcode.php
+++ trunk/lng-lat-from-zipcode.php
@@ -3,38 +3,70 @@
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 1);
 
-// Settings, change these if you want.
+// Globals.
 define('DRD_ROOT', realpath(dirname(__FILE__)) . '/');
 define('DRD_FILE_IN', DRD_ROOT . 'danish_regional_data.txt');
 define('DRD_FILE_OUT', DRD_ROOT . 'danish_regional_data_lng_lat.txt');
 
-class LngLatFromZipcode {
+/**
+ * Reads the file danish_regional_data.txt and creates a new file
+ * danish_regional_data_lng_lat.txt with latitude and longitude added to each
+ * line.
+ *
+ * Uses the python script python multimap-danish-zipcode-search.py to get the
+ * latitude and longitude.
+ */
+class DRD_LngLatFromZipcode {
+    /**
+     * Constructor.
+     */
     function __construct() {
         $this->cleanOutputFile();
         $this->parseRows($this->readRows());
     }
 
+    /**
+     * Deletes content before writing to file.
+     */
     function cleanOutputFile() {
         file_put_contents(DRD_FILE_OUT, '');
     }
-    
+
+    /**
+     * Returns all rows from file.
+     * 
+     * @return array
+     */
     function readRows() {
-        return explode("\n", file_get_contents(DRD_FILE_IN));
+        return array_filter(explode("\n", file_get_contents(DRD_FILE_IN)));
     }
 
+    /**
+     * Loops over rows and writes to file.
+     * 
+     * @param array $rows
+     */
     function parseRows($rows) {
         foreach($rows as $row) {
-            if (empty($row)) continue;
             $zipcode = $this->parseRow($row);
             $lng_lat = trim(`python multimap-danish-zipcode-search.py $zipcode`);
             file_put_contents(
                 DRD_FILE_OUT,
                 "$row $lng_lat\n",
                 FILE_APPEND);
-            sleep(1);
+            sleep(1); // Don't hit mapfilter api too hard.
         }
     }
 
+    /**
+     * Returns zipcode of a single row.
+     * 
+     * @staticvar string $s
+     * @staticvar string $n
+     * @staticvar string $nn
+     * @param string $row
+     * @return string
+     */
     function parseRow($row) {
         static $s = "[\ ]+";
         static $n = "[0-9]+";
@@ -47,4 +79,4 @@ class LngLatFromZipcode {
     }
 }
 
-new LngLatFromZipcode;
+new DRD_LngLatFromZipcode;
